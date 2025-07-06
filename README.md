01870a9f2421c3179394b69dc49d5fca78b526f6
Emiliano1709 <emiliano.lopez1417@gmail.com>
2025-07-05 21:49:21 -0600

    Commit limpio

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Diemi_JS
-El objetivo es hacer un control de distribución de invitaciones y de listas de invitados a eventos como bodas, cumpleaños, etc. La boda de Diana y Emiliano es el objetivo principal.
+Desarrollado por SERGIO EMILIANO LOPEZ BAUTISTA [10 06 2025]
+**Cualquier otro commit realizado por alguien más es un uso indebido de las versiones iniciales**
+
+Esta es la versión HTML, CSS, JS del proyecto Diemi que originalmente está hecho con Python.
+
+= V.1.0.0 =
+El maquetado del proyecto incluye:
+    - Despliegues de iframe de
+        = sitio diseñado en Canva
+        = Mapas interactivos de Google Maps
+    - Comunicación con una hoja de cálculo de Google usando Apps Script
+    - Audio insertado directamente para una experiencia directa con el usuario
+
+= V.1.0.1 =
+Se agrega la función de scroll para dispositivos moviles 
+
+= V.1.0.2 =
+En la versión anterior no funcionó el scroll
---
+++ cuenta.js
@@ -0,0 +1,25 @@
+const fechaObjetivo = new Date("2025-10-04T12:00:00").getTime();
+
+const actualizaContador = () => {
+    const ahora = new Date().getTime();
+    const diferencia = fechaObjetivo - ahora;
+
+    if (diferencia < 0) {
+        document.getElementById("contador").innerHTML = "<h1>¡Es hoooooooooooy!</h1>";
+        clearInterval(timer);
+        return;
+    }
+
+    const dias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
+    const horas = Math.floor((diferencia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutos = Math.floor((diferencia % (1000 * 60 * 60)) / (1000 * 60));
+    const segundos = Math.floor((diferencia % (1000 * 60)) / 1000);
+
+    document.getElementById("dias").textContent = dias.toString().padStart(2, '0');
+    document.getElementById("horas").textContent = horas.toString().padStart(2, '0');
+    document.getElementById("minutos").textContent = minutos.toString().padStart(2, '0');
+    document.getElementById("segundos").textContent = segundos.toString().padStart(2, '0');
+};
+
+const timer = setInterval(actualizaContador, 1000);
+actualizaContador(); // llamar una vez al inicio de todo xd
