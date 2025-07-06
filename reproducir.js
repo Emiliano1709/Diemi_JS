c2d7f31f26de490cbe454b1fd5e7ac0ff76d6b79
Emiliano1709 <emiliano.lopez1417@gmail.com>
2025-07-06 00:40:49 -0600

    play y pausa

--- reproducir.js
+++ reproducir.js
@@ -1,3 +1,4 @@
+/*
 const boton = document.getElementById('play-boton');
 const audio = document.getElementById('audio');
 
@@ -5,3 +6,21 @@ boton.addEventListener('click', () => {
   audio.play();
   boton.disabled = true; // Desactiva el botón después de hacer clic
 });
+*/
+
+  const audio = document.getElementById('audio');
+  const boton = document.getElementById('play-boton');
+  const icono = document.getElementById('icono-play');
+
+  let estaReproduciendo = false;
+
+  boton.addEventListener('click', () => {
+    if (estaReproduciendo) {
+      audio.pause();
+      icono.src = "play.PNG";
+    } else {
+      audio.play();
+      icono.src = "pausa.PNG";
+    }
+    estaReproduciendo = !estaReproduciendo;
+  });
